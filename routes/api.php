6ffe72334bea794b7e604a630b9df77b93b65d63
Eduardo Silva <eduardo@dudstecnologia.com.br>
2019-08-15 23:18:45 -0400

    Implicit Grant

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::prefix('v1')->group(function () {
+Route::middleware('auth:api')->prefix('v1')->group(function () {
     /*
     Route::get('/products', 'ProductsController@index');
     Route::post('/products', 'ProductsController@store');
